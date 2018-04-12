b161042af8b7d5f8f9d21b926fe01acb690bb599
missetand <c.nielsen39@gmail.com>
2018-04-12 15:51:53 +0200

    added 3rd party libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './_services/alertify.service';
+
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 
 
@@ -23,10 +26,12 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
-    AuthService
+    AuthService,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
