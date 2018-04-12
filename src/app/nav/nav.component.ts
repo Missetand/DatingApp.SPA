26e246db8c8cc3dab20c8afaed4267616f894e70
missetand <c.nielsen39@gmail.com>
2018-04-12 13:32:21 +0200

    Error handling

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       console.log('Login successfull');
     }, error => {
-      console.log('failed to loggin');
+      console.log(error);
     });
   }
   logout() {
