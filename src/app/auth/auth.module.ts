154317638de5f3d69605f7a8ad9451fc959b0747
missetand <c.nielsen39@gmail.com>
2018-04-15 00:52:09 +0200

    UI Update

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig({
+    tokenName: 'token',
+    tokenGetter: (() => localStorage.getItem('token')),
+    // tslint:disable-next-line:whitespace
+    globalHeaders: [{'Content-Type':'application/json'}],
+  }), http, options);
+}
+
+@NgModule({
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    }
+  ]
+})
+export class AuthModule { }
